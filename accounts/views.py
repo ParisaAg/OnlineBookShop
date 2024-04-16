28d374a9a40a02c8570e751ba52afa246771d936
ParisaAg <parisaaghaei17@gmail.com>
2024-04-16 09:17:36 +0330

    crispy-forms added

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,9 @@ from django.views import generic
 from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
 # Create your views here.
+
+
+
 class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration/signup.html'
--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+class HomePageTest(TestCase):
+    def test_home_page_url_by_name(self):
+        response=self.client.get(reversed('home'))
+        self.assertEqual((response.status_code,200))
+
+    def test_home_page_url_by_name(self):
+        response=self.client.get(reversed('home'))
+        self.assertEqual((response.status_code,200))
