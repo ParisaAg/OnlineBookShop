bba1cb80ba2f2f2c56fa01d9684114da962dcb2f
ParisaAg <parisaaghaei17@gmail.com>
2024-04-30 07:16:12 +0330

    add detail page

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Book(models.Model):
@@ -8,3 +9,6 @@ class Book(models.Model):
     price=models.DecimalField(decimal_places=2,max_digits=5)
     def __str__(self):
         return f'{self.title} by : {self.author}'
+
+    def get_absolute_url(self):
+        return reverse('book_detail',args=[self.id])
