89605cb2bc7a22ee4067a57636ef67db7f1ae038
ParisaAg <parisaaghaei17@gmail.com>
2024-05-28 15:41:44 +0330

    covers added

--- books/models.py
+++ books/models.py
@@ -7,6 +7,7 @@ class Book(models.Model):
     author=models.CharField(max_length=255)
     content=models.TextField()
     price=models.DecimalField(decimal_places=2,max_digits=5)
+    cover=models.ImageField(upload_to='covers/',blank=True)
     def __str__(self):
         return f'{self.title} by : {self.author}'
 
